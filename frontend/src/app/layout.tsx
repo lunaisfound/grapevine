511564fc5f122d73170bd99b7f24d9a29e199632
Giao Bui <118485842+giaobui2204@users.noreply.github.com>
2025-04-02 19:59:38 -0400

    Update the css for nav bar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react";
-import NavigationBar from "./components/NavigationBar";
+import NavigationBar from "../components/NavigationBar";
 
 interface LayoutProps {
   children: ReactNode;
