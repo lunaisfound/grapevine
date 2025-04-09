1f5b39ee069443fb922cc261da6ef98edb19c3aa
Anhakian <anhnguyenngochoang2003@gmail.com>
2025-04-09 02:17:02 -0400

    fix add business function

--- frontend/src/app/(auth)/business-name/page.tsx
+++ frontend/src/app/(auth)/business-name/page.tsx
@@ -73,7 +73,7 @@ export default function BusinessNamePage() {
                   borderRadius: "8px",
                 }}
               >
-                "CONTINUE"
+                CONTINUE
               </Button>
             </Space>
           </Col>
